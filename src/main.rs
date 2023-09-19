f6652b5acd4769ef90b41ecbaf325da48d1994c9
Benjamin Massey <benjamin.w.massey@gmail.com>
2023-09-18 18:18:31 -0700

    Added score entry: horizontal :o

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,16 @@ fn main() {
 
 #[derive(Default)]
 struct CompeteApp {
-    player_edit_result: Vec<String>,
+    player_edit: Vec<String>,
+    score_edit: Vec<String>,
 }
 
 impl CompeteApp {
-    fn new(_cc: &eframe::CreationContext<'_>) -> Self { CompeteApp {
-        player_edit_result: vec![String::new(), String::new(), String::new(), String::new()],}
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self { 
+        CompeteApp {
+            player_edit: vec![String::new(), String::new(), String::new(), String::new()],
+            score_edit: vec![String::new(), String::new(), String::new(), String::new()],
+        }
     }
 }
 
@@ -25,15 +29,23 @@ impl eframe::App for CompeteApp {
         egui::CentralPanel::default().show(ctx, |cui| {
             cui.heading("Kart JSON Editor");
             for i in 0..4 {
-                let response = cui.add(
-                    egui::TextEdit::singleline(&mut self.player_edit_result[i])
-                        .hint_text("Enter player name...")
-                );
+                cui.horizontal(|hui| {
+                    hui.add(
+                        egui::TextEdit::singleline(&mut self.player_edit[i])
+                            .hint_text("Enter player name...")
+                    );
+                    hui.add(
+                        egui::TextEdit::singleline(&mut self.score_edit[i])
+                            .hint_text("Score...")
+                    );
+                });
+                
             }
             if cui.add(egui::Button::new("Submit Match")).clicked() {
                 for i in 0..4 {
-                    print!("{} ", self.player_edit_result[i]);
-                    self.player_edit_result[i] = String::new();
+                    print!("{}: {} ; ", self.player_edit[i], self.score_edit[i]);
+                    self.player_edit[i] = String::new();
+                    self.score_edit[i] = String::new();
                 }
                 println!("");
             }
